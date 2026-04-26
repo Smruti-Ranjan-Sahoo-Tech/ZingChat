9c71e0e2b160d56d40f3ffe542380ff30dfe4339
Smruti Ranjan Sahoo <s.r.sahoo370@gmail.com>
2026-04-26 10:31:04 +0530

    client added

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,32 @@
 console.log("Hello via Bun!");
+import express from "express"
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+  cors:{
+    origin:"*"
+  },
+});
+
+const ROOM="group";
+
+io.on("connection",(socket)=>{
+    console.log("A user connected:", socket.id);
+    socket.on("joinRoom",async(userName:string)=>{
+           console.log(`${userName} is joining the group.`)
+     })
+     socket.on("disconnect",()=>{
+        for()
+     })
+})
+
+
+
+app.get('/', (req, res) => {
+  res.send("Hello Express on Bun");
+});
+
+server.listen(8000);
